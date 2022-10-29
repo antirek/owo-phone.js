ab3da477fd07d9f0a60a59fb6e3ff6bc7715bdf4
sergey <110@mobilon.ru>
2022-10-29 23:57:10 +0700

    fix

---
+++ v2/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+NODE_ENV=production npm run build
--- v2/vue.config.js
+++ v2/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/v2/dist/'
+      ? '/owo-phone.js/v2/dist/'
       : '/'
 }
